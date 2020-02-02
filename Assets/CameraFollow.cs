8ee7c669288dbe39c717642401b108a6d93cdc69
Devlin McClure <mccluredt@gmail.com>
2020-02-02 14:10:31 -0500

    Add camera follow script

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] float FollowSpeed = 2f;
+    [SerializeField] Transform playerA;
+    [SerializeField] Transform playerB;
+
+    private void Update()
+    {
+
+        Vector3 newPosition = (playerA.transform.position + playerB.transform.position) / 2;
+        newPosition.z = -10;
+        transform.position = Vector3.Slerp(transform.position, newPosition, FollowSpeed * Time.deltaTime);
+    }
+}
