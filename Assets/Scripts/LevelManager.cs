eecb6f79b66bdcb81123522a4447306f4418eb20
Devlin McClure <mccluredt@gmail.com>
2020-02-02 12:34:21 -0500

    Fix animation

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class LevelManager : MonoBehaviour
 {
     public static LevelManager instance;
-    [SerializeField] GameObject sceneObjects;
+    //[SerializeField] GameObject sceneObjects;
     [SerializeField] Animator transition;
     [SerializeField] float transitionTime = 1f;
 
@@ -21,7 +21,7 @@ public class LevelManager : MonoBehaviour
             instance = this;
         }
 
-        DontDestroyOnLoad(this);
+        //DontDestroyOnLoad(this);
     }
 
     public IEnumerator ChangeLevel(string levelToLoad)
@@ -30,34 +30,44 @@ public class LevelManager : MonoBehaviour
 
         yield return new WaitForSeconds(transitionTime);
 
-        sceneObjects.SetActive(false);
+        //sceneObjects.SetActive(false);
         //SceneManager.LoadScene(levelToLoad, LoadSceneMode.Additive);
-        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(levelToLoad, LoadSceneMode.Additive);
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(levelToLoad);
 
         // Wait until the asynchronous scene fully loads
         while (!asyncLoad.isDone)
         {
             yield return null;
         }
-        //yield return null;
-    }
 
-    public IEnumerator EndLevel(string currentLevel)
-    {
-        transition.SetTrigger("Start");
-
-        yield return new WaitForSeconds(transitionTime);
-
-        AsyncOperation asyncUnload = SceneManager.UnloadSceneAsync(currentLevel);
-
-        // Wait until the asynchronous scene fully loads
-        while (!asyncUnload.isDone)
-        {
-            yield return null;
-        }
-        
         Resources.UnloadUnusedAssets();
-        sceneObjects.SetActive(true);
         //yield return null;
     }
+
+    // public IEnumerator EndLevel(string currentLevel)
+    // {
+    //     transition.SetTrigger("Start");
+
+    //     yield return new WaitForSeconds(transitionTime);
+
+    //     AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("LevelSelect");
+
+    //     // Wait until the asynchronous scene fully loads
+    //     while (!asyncLoad.isDone)
+    //     {
+    //         yield return null;
+    //     }
+
+    //     AsyncOperation asyncUnload = SceneManager.UnloadSceneAsync(currentLevel);
+
+    //     // Wait until the asynchronous scene fully loads
+    //     while (!asyncUnload.isDone)
+    //     {
+    //         yield return null;
+    //     }
+        
+    //     Resources.UnloadUnusedAssets();
+    //     //sceneObjects.SetActive(true);
+    //     //yield return null;
+    // }
 }
