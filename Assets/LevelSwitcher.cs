e9339537a7d00728b35b52afc6f480b1e0f69944
Devlin McClure <mccluredt@gmail.com>
2020-02-01 20:16:40 -0500

    First pass of Level Switching

---
+++ Assets/LevelSwitcher.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSwitcher : MonoBehaviour
+{
+    [SerializeField]string levelToLoad;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        StartCoroutine(LevelManager.instance.ChangeLevel(levelToLoad));
+    }
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] GameObject sceneObjects;
+    public static LevelManager instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            GameObject.Destroy(instance);
+        }
+        else
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(this);
+    }
+
+    public IEnumerator ChangeLevel(string levelToLoad)
+    {
+        sceneObjects.SetActive(false);
+        //SceneManager.LoadScene(levelToLoad, LoadSceneMode.Additive);
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(levelToLoad, LoadSceneMode.Additive);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+        //yield return null;
+    }
+
+    public IEnumerator EndLevel(string currentLevel)
+    {
+        AsyncOperation asyncUnload = SceneManager.UnloadSceneAsync(currentLevel);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncUnload.isDone)
+        {
+            yield return null;
+        }
+        Resources.UnloadUnusedAssets();
+        sceneObjects.SetActive(true);
+        //yield return null;
+    }
+}
