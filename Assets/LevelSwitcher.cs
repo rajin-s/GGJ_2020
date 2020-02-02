6efa3861bf01f9c247fe3d9cfb8f5733915acae0
Devlin McClure <mccluredt@gmail.com>
2020-02-02 15:09:47 -0500

    Finished level selector

--- Assets/LevelSwitcher.cs
+++ Assets/LevelSwitcher.cs
@@ -5,25 +5,26 @@ using UnityEngine;
 public class LevelSwitcher : MonoBehaviour
 {
     [SerializeField] LevelManager manager;
-    [SerializeField] string levelToLoad;
-    [SerializeField] string playerTag;
-    [SerializeField] string button;
-    [SerializeField] bool inside;
+    [SerializeField] private string levelToLoad;
+    [SerializeField] private string playerTag;
+    [SerializeField] private string button;
+    [SerializeField] private bool inside = false;
     private bool canTransition = true;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         //Debug.Log("Ahhhhhhhhhhhhhhhhhhhhhhhhh");
-        if (other.gameObject.CompareTag(tag))
+        if (other.gameObject.CompareTag(playerTag))
         {
             //Debug.Log("Ahhhhhhhhhhhhhhhhhhhhhhhhh");
+            Debug.Log(other.tag);
             inside = true;
         }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag(tag))
+        if (other.gameObject.CompareTag(playerTag))
         {
             inside = false;
         }
@@ -31,8 +32,9 @@ public class LevelSwitcher : MonoBehaviour
     
     private void Update()
     {
-        if (Input.GetButtonDown(button) && canTransition)
+        if (Input.GetButtonDown(button) && canTransition && inside)
         {
+            Debug.Log(levelToLoad);
             canTransition = false;
             ChangeLevel();
         }    
